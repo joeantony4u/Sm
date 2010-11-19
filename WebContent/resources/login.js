5ca311b3e83c5b893d6642c39c93fec24513e4e4
Binu <binu.mymailbox@gmail.com>
2010-11-20 03:47:12 +0530

    Signed-off-by: Binu <binu.mymailbox@gmail.com>

--- WebContent/resources/login.js
+++ WebContent/resources/login.js
@@ -3,29 +3,15 @@ var easings = [ 'easeInQuad', 'easeOutQuad', 'easeInCubic', 'easeOutCubic',
     			'easeInSine', 'easeOutSine', 'easeInExpo', 'easeOutExpo',
     			'easeInCirc', 'easeOutCirc', 'easeInElastic', 'easeOutElastic',
     			'easeInBack', 'easeOutBack', 'easeInBounce', 'easeOutBounce' ];
-    	
-
-	$(function() {
-		$('#container-4').tabs({
-			fxFade : true,
-			fxSpeed : 'fast'
-		});
-
-	});
+$.fn.reverse = [].reverse;
 
 	$(document).ready(function() {
-		$.fn.reverse = [].reverse;
-		$("#slider").easySlider({
-			//auto: true,
-			continuous : true
-		});
-		$("a", "#nextBtn").click();
 		easingsOn('div.easing-visibility', 'show', easings[1]);
 		$('#container img').fadeIn(3000);
 	});
 
 	function easingsOn(selector, visibility, easing) {
-		var elements = visibility == 'show' ? $(selector) : $(selector)
+		var elements = (visibility == 'show') ? $(selector) : $(selector)
 				.reverse();
 		elements.each(function(i) {
 			var $de = $(this);
@@ -36,31 +22,29 @@ var easings = [ 'easeInQuad', 'easeOutQuad', 'easeInCubic', 'easeOutCubic',
 			}, 200 * (i + 1));
 		});
 	}
-
-	function login() {
-		easingsOn('div.easing-visibility', 'hide', easings[0]);
-		var bimg = $('#container img'); 
-		bimg.fadeOut('slow', function() {
-			bimg.attr('src', './images/progress.gif').fadeIn('slow', authenticate);
+	
+	function repaintImage(selector, withURL, callback) {
+		$(selector).fadeOut('slow', function() {
+			$(this).attr('src', withURL).fadeIn('slow', callback);
 		});
+	}
+
+	function preLogin() {
+		easingsOn('div.easing-visibility', 'hide', easings[0]);
+		repaintImage('#container img', './images/progress.gif', authenticate);
 	}
 	
 	function authenticate() {
-		var bimg = $('#container img');
 		Authenticate.login($('#username').val(), $('#password').val(), function(res) {
 			if(res == 'failure') {
-				bimg.fadeOut('slow', function() {
-					bimg.attr('src', './images/puppy.gif').fadeIn('slow');
+				repaintImage('#container img', './images/puppy.gif', function() {
 					easingsOn('div.easing-visibility', 'show', easings[1]);
 					$('#login-button').html('relogin').addClass('button-err');
-				}); 
+				});
 				//TODO: Forward to next page
 			} else {
-				bimg.fadeOut('slow', function() {
-					bimg.attr('src', './images/dog.gif').fadeIn('slow');
-				});
-				// Testing - Need to remove the above code
-				bimg.remove();
+				repaintImage('#container img', './images/dog.gif', function() {	});
+				$('#container img').remove();
 				$('<div/>').attr('id', 'chart-container').appendTo($('#container'));
 				plotCommonChart();
 			}
